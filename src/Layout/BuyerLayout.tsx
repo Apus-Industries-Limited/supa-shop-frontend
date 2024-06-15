3e736f0dc79c1867cd668e9907276d60aa552d3e
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2024-06-15 08:41:58 +0000

    restructuring

--- src/Layout/BuyerLayout.tsx
+++ src/Layout/BuyerLayout.tsx
@@ -1,14 +1,16 @@
-import { Outlet } from "react-router-dom"
-import { BuyerProvider } from "../context/BuyerContext"
+import { Outlet } from "react-router-dom";
+import { BuyerProvider } from "../context/BuyerContext";
+import Header from "../components/Header";
 
 const BuyerLayout = () => {
   return (
     <BuyerProvider>
       <main className="bg-[#eeeeee] text-[#2a2a2a] dark:bg-[#2a2a2a] dark:text-[#eeeeee] min-h-screen max-w-full">
+        <Header/>
         <Outlet/>
       </main>
     </BuyerProvider>
   )
 }
 
-export default BuyerLayout
+export default BuyerLayout;
