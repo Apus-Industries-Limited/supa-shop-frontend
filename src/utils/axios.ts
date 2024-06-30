46c5ec1230c9e17e92b5d96f2a4c8f397aba810d
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2024-06-30 18:39:40 +0100

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,15 +1,15 @@
 import axios from "axios"
 
-const LOCAL_URL:string = 'http://localhost:3500'
+//const LOCAL_URL:string = 'http://localhost:3500'
 const DEV_URL: string = 'https://supa-shop-backend.onrender.com'
 
 
 export default axios.create( {
-      baseURL: LOCAL_URL,
+      baseURL: DEV_URL,
 })
 
 export const axiosPrivate = axios.create({
-      baseURL: LOCAL_URL,
+      baseURL: DEV_URL,
       headers: {
             "Content-Type":"application/json"
       }
