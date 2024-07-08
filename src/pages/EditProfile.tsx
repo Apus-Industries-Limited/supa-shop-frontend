a1a17ef63e495044d6f661a569704faee1bc180c
dev_apus <folorunsopraise580@gmail.com>
2024-07-08 17:32:01 +0100

    Profile completed

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -2,7 +2,7 @@
 import { Avatar, Button, Input, useDisclosure } from "@nextui-org/react"
 import useBuyerContext from "../hooks/useBuyerContext"
 import profile from "../assets/image/avatar.jpg"
-import { BsAt, BsPencilFill, BsPerson, BsTelephoneFill } from "react-icons/bs"
+import { BsAt, BsPerson, BsTelephoneFill } from "react-icons/bs"
 import { useState } from "react"
 import { EditProfileData } from "../types/Formtypes"
 import { CiLocationOn } from "react-icons/ci"
@@ -25,11 +25,14 @@ const EditProfile = () => {
     address: user.address[0]
   });
   const [loading, setLoading] = useState<boolean>(false);
-  console.log(user)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData , [e.target.name]: e.target.value });
   };
+
+  const handleCancle = () => {
+    navigate('/profile')
+  }
 
   const updateUser = async () => {
     try {
@@ -40,7 +43,6 @@ const EditProfile = () => {
         }
       })
       const result = await res.data.user
-      console.log(result)
       setUser({...user, name:result.name, username:result.username, phone_number:result.phone_number,address:result.address})
       toastMsg("success", res.data.message)
       navigate('/profile')
@@ -68,23 +70,34 @@ const EditProfile = () => {
   return (
     <div>
       {/*Tablet and  Desktop View */}
-      <div className="min-h-screen hidden md:flex border justify-center items-center w-full">
-        <div className="container">
-          <div className="relative  mb-10 w-[32rem] mx-auto">
-            <Avatar src={user?.dp ? user.dp : profile} className="w-48 h-48 relative mx-auto" />
-            <div className="absolute bg-primary bottom-0 text-white p-4 rounded-full right-40 hover:bg-[#ffa14d]" role="button" onClick={onOpen}>
-              <BsPencilFill size={26} />
+      <div className="min-h-screen hidden md:block mx-auto py-12 w-full">
+        <p className="text-lg capitalize">user profile</p>
+        <hr className="w-[6rem] my-3 border text-black"/>
+        <div className="container mx-auto">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center">
+              <Avatar src={user?.dp ? user.dp : profile} className="w-28 h-28 me-3" />
+              <div>
+                <p className="text-2xl capitalize">{formData.name}</p>
+                <p className="text-neutral-500 text-tiny">{user?.bio ? user.bio : "Our esteem user" }</p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <Button color="primary" className="me-2 font-bold" onClick={onOpen}>Upload New Photo</Button>
+              <Button color="default" className="ms-2 font-bold">Delete</Button>
             </div>
           </div>
 
+          <hr className="border my-10" />
           <div className="grid grid-cols-2 gap-[5rem] my-5">
-            <Input color="primary" type="" variant="underlined" onChange={handleChange} endContent={<BsPerson className="text-primary" size={18}/>} name="name" placeholder="Enter your name" value={formData.name}/>
-            <Input color="primary" type="" variant="underlined" onChange={handleChange} endContent={<BsAt className="text-primary" size={18}/>} name="username" placeholder="Enter preferred username" value={formData.username} />
-            <Input color="primary" type="" variant="underlined" onChange={handleChange} endContent={<BsTelephoneFill className="text-primary" size={18}/>} name="phone_number" placeholder="Enter preferred phone number" value={formData.phone_number} />
-            <Input color="primary" type="" variant="underlined" onChange={handleChange} endContent={<CiLocationOn className="text-primary" size={18}/>} name="address" placeholder="Enter your address" value={formData.address}/>
+            <Input labelPlacement="outside" label="Name" type="" onChange={handleChange} startContent={<BsPerson className="text-primary" size={18}/>} name="name" placeholder="Enter your name" value={formData.name}/>
+            <Input labelPlacement="outside" label="Username" type="" onChange={handleChange} startContent={<BsAt className="text-primary" size={18}/>} name="username" placeholder="Enter preferred username" value={formData.username} />
+            <Input labelPlacement="outside" label="Phone Number" type="" onChange={handleChange} startContent={<BsTelephoneFill className="text-primary" size={18}/>} name="phone_number" placeholder="Enter preferred phone number" value={formData.phone_number} />
+            <Input labelPlacement="outside" label="Address" type="" onChange={handleChange} startContent={<CiLocationOn className="text-primary" size={18}/>} name="address" placeholder="Enter your address" value={formData.address}/>
           </div>
-          <div className="w-1/4 mx-auto mt-12">
-            <Button isLoading={loading} isDisabled={loading} color="primary" className="w-full" onClick={updateUser}>Update</Button>
+          <div className="flex items-center justify-end mx-auto mt-12">
+            <Button color="default" className="me-2 font-bold" onClick={handleCancle}>Cancel</Button>
+            <Button isLoading={loading} isDisabled={loading} color="primary" className="ms-2" onClick={updateUser}>Save Changes</Button>
           </div>
         </div>
       </div>
